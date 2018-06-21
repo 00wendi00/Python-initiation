a89458560781df575d96498775a7a30ab607eab3
wendi <842876912@qq.com>
2018-06-22 01:09:02 +0800

    复制, 浅拷贝, 深拷贝.  不可变对象

--- wendi/test_more.py
+++ wendi/test_more.py
@@ -4,15 +4,29 @@
 # @Author: Wade Cheung
 # @Date  : 2018/6/14
 # @Desc  :  更多内容 : 1.特殊方法, 2.单语句块, 3.列表综合,
-# 4.可变参数列表-- *args接收元组列表, **args接收字典
+# 4.可变参数列表-- *args接收元组列表, **args接收字典 . 实参和形参的写法.
 # 5.lambda表达式
-# 6.exec执行语句 ,  eval执行语句
+# 6.exec执行语句 ,  eval执行计算
 # 7.repr函数和反引号
 import os
 
 listone = [2, 3, 4]
 listtwo = [2 * i for i in listone if i > 2]
 print(listtwo)
+
+
+# 定义的时候 * 的作用 将位置实参 装配成元组，** 的作用 将关键字实参 装配成字典
+def test3(x, y, z):
+    print(x, y, z)
+
+
+x1 = [1, 2, 3]
+x2 = ('q', 'w', 'e')
+y = {'x': 1, 'y': 2, 'z': 3}
+# 实参的写法.
+test3(*x1)
+test3(*x2)
+test3(**y)
 
 
 def powersum(power, *args):
