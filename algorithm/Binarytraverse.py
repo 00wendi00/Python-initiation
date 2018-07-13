678a94f698fe2718f729abdd727129685c0ef4c5
wendi <842876912@qq.com>
2018-07-14 00:02:05 +0800

    二叉树的三种遍历方式, 先序NLR, 中序LNR, 后序LRN.   递归实现  +  遍历实现

--- algorithm/Binarytraverse.py
+++ algorithm/Binarytraverse.py
@@ -9,18 +9,40 @@ from algorithm.BinarySortTree import BinarySortTree
 
 
 # 先序遍历NLR
-def node_left_right(node):
+def node_left_right1(node):
     if node:
-        return [node.data] + node_left_right(node.left) + node_left_right(node.right)
+        return [node.data] + node_left_right1(node.left) + node_left_right1(node.right)
+    else:
+        return []
+
+
+# 先序遍历NLR
+def node_left_right2(node):
+    l = []
+    stack = []
+    while stack or node:
+        while node:
+            l.append(node.data)
+            stack.append(node)
+            node = node.left
+        node = stack.pop()
+        node = node.right
+
+    return l
+
+
+# 中序遍历LNR
+def left_node_right1(node):
+    if node:
+        return left_node_right1(node.left) + [node.data] + left_node_right1(node.right)
     else:
         return []
 
 
 # 中序遍历LNR
-def left_node_right(bst):
+def left_node_right2(node):
     l = []
     stack = []
-    node = bst._root
     while stack or node:
         while node:
             stack.append(node)
@@ -33,11 +55,26 @@ def left_node_right(bst):
 
 
 # 后序遍历LRN
-def left_right_node(node):
+def left_right_node1(node):
     if node:
-        return left_right_node(node.left) + left_right_node(node.right) + [node.data]
+        return left_right_node1(node.left) + left_right_node1(node.right) + [node.data]
     else:
         return []
+
+
+# 后序遍历LRN , 最后reverse
+def left_right_node2(node):
+    l = []
+    stack = [node]
+    while stack:
+        node = stack.pop()
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+        l.append(node.data)
+    l.reverse()   # append 的实际顺序为中右左 . 翻转为左右中.
+    return l
 
 
 # 将列表insert进binary
@@ -52,8 +89,11 @@ for i in bs_tree:
 
 print('\n')
 
-print(node_left_right(bs_tree._root), '先序\n')
+print(node_left_right1(bs_tree._root), '先序1\n')
+print(node_left_right2(bs_tree._root), '先序2\n')
 
-print(left_node_right(bs_tree), '中序\n')
+print(left_node_right1(bs_tree._root), '中序1\n')
+print(left_node_right2(bs_tree._root), '中序2\n')
 
-print(left_right_node(bs_tree._root), '后序\n')
+print(left_right_node1(bs_tree._root), '后序1\n')
+print(left_right_node2(bs_tree._root), '后序2\n')
