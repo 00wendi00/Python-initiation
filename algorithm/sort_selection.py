28304d96f0385a0cf021bf142e2fe3cf46491f31
wendi <842876912@qq.com>
2018-07-18 22:14:26 +0800

    选择排序, 每次找出最大数的位置, 将最大数交换到对应位置 --> 比较次数未变, 减少了交换次数

--- algorithm/sort_selection.py
+++ algorithm/sort_selection.py
@@ -3,7 +3,7 @@
 # @File  : sort_selection.py
 # @Author: Wade Cheung
 # @Date  : 2018/7/18
-# @Desc  : 选择排序, 每次找出最大数的位置, 将最大数交换到对应位置 --> 比较次数未变, 减少了交换次数
+# @Desc  : 选择排序, 每次找出最大数的位置, 将最大数交换到对应位置 与sort_bubble比较 --> 比较次数未变, 减少了交换次数
 
 
 def selection_sort(lis):
@@ -19,17 +19,19 @@ def selection_sort(lis):
             if lis[location] > lis[positionOfMax]:
                 positionOfMax = location
 
-        exchangeTime += 1
-        # 将最大数交换到对应位置
-        temp = lis[fileslot]
-        lis[fileslot] = lis[positionOfMax]
-        lis[positionOfMax] = temp
+        # 最大数的位置和此位置不相等 --> 则交换
+        if positionOfMax != fileslot:
+            exchangeTime += 1
+            # 将最大数交换到对应位置
+            temp = lis[fileslot]
+            lis[fileslot] = lis[positionOfMax]
+            lis[positionOfMax] = temp
 
     print(compareTimes)
     print(exchangeTime)
 
 
 # alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-alist = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
+alist = [5, 30, 20, 40, 90, 50, 10, 60, 70, 80, 100, 110]
 selection_sort(alist)
 print(alist)
