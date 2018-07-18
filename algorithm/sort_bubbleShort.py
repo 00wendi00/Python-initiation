bc622a8184fa3e351fb866bcb67a286015cb234f
wendi <842876912@qq.com>
2018-07-18 22:13:52 +0800

    短冒泡排序, 与sort_bubble比较 --> 减少了比较次数,交换次数未变

--- algorithm/sort_bubbleShort.py
+++ algorithm/sort_bubbleShort.py
@@ -3,21 +3,23 @@
 # @File  : sort_bubbleShort.py
 # @Author: Wade Cheung
 # @Date  : 2018/7/18
-# @Desc  : 短冒泡排序, 与sort_bubble比较. 减少了比较次数,交换次数未变
+# @Desc  : 短冒泡排序, 与sort_bubble比较 --> 减少了比较次数,交换次数未变
 
 
 def shortBubbleSort(alist):
-    exchanges = True  # 是否提前停止循环的标记
+    exchanges = True  # 是否继续循环的标记
     passnum = len(alist) - 1
     compareTimes = 0  # 记录比较次数
     exchangeTime = 0  # 记录交换次数
-    # 当都比较了一次 & 继续循环(发生了交换)
+    # 当此轮未比较完 & 继续循环(发生了交换)
     while passnum > 0 and exchanges:
         exchanges = False
         for i in range(passnum):
             compareTimes += 1
+
             if alist[i] > alist[i + 1]:
                 exchangeTime += 1
+
                 exchanges = True
                 temp = alist[i]
                 alist[i] = alist[i + 1]
@@ -28,6 +30,6 @@ def shortBubbleSort(alist):
     print(exchangeTime)
 
 
-lis = [20, 30, 40, 90, 50, 10, 60, 70, 80, 100, 110, 5]
+lis = [5, 30, 20, 40, 90, 50, 10, 60, 70, 80, 100, 110]
 shortBubbleSort(lis)
 print(lis)
