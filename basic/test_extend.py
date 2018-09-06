57f118451a9ea1a5d487069312053e0a8a950810
wendi <842876912@qq.com>
2018-09-06 22:25:42 +0800

    多继承, 基类的方法名相同时的执行情况 -- MRO列表

--- basic/test_extend.py
+++ basic/test_extend.py
@@ -3,8 +3,11 @@
 # @File  : test_extend.py
 # @Author: Wade Cheung
 # @Date  : 2018/9/6
-# @Desc  : 多继承, 基类的方法名相同时的执行情况
+# @Desc  : 多继承, 基类的方法名相同时的执行情况 -- MRO列表
 
+
+# 属性与方法的遍历 : MRO列表-- 所有基类的线性顺序列表 ,
+# C3线性化算法来实现 : 子类先于父类被检查, 多个父类根据顺序,
 
 class A:
     def haha(self):
